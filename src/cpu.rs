7036b67a81826fc47c0e3aca6856c070dbc98490
vzwGrey <vzwgrey@gmail.com>
2022-02-15 17:28:50 +0100

    feat: jnz, ret, cpi, inx, dcr, push, pop

--- src/cpu.rs
+++ src/cpu.rs
@@ -74,6 +74,16 @@ impl Register {
     }
 }
 
+/// 8080 CPU flags
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Flag {
+    C = 1 << 0,
+    P = 1 << 2,
+    // AC = 1 << 4,
+    Z = 1 << 6,
+    S = 1 << 7,
+}
+
 /// 8080 CPU state
 #[derive(Debug, Default)]
 pub struct Cpu8080 {
@@ -93,7 +103,12 @@ pub struct Cpu8080 {
 impl Cpu8080 {
     /// Create a new, blank CPU state
     pub fn new() -> Self {
-        Self::default()
+        Self {
+            pc: 0,
+            sp: 0,
+            gprs: [0; 7],
+            flags: 1 << 1,
+        }
     }
 
     /// Execute a single instruction with the current CPU state
@@ -118,13 +133,43 @@ impl Cpu8080 {
                 debug_println!("JUMP ${:04X}", addr);
             }
 
+            // JNZ
+            (0xC, 0x2) => {
+                let addr = self.fetch_word(io);
+
+                debug_println!("JNZ ${:04X}", addr);
+                if !self.get_flag(Flag::Z) {
+                    self.jump(addr);
+                }
+            }
+
             // CALL
             (0xC..=0xF, 0xD) => {
                 let addr = self.fetch_word(io);
-                self.push_word(io, self.pc);
+                self.push(io, self.pc);
                 self.jump(addr);
 
                 debug_println!("CALL ${:04X}", addr);
+            }
+
+            // RET
+            (0xC, 0x9) => {
+                debug_println!("RET");
+
+                let addr = self.pop(io);
+                self.jump(addr);
+            }
+
+            // CPI
+            (0xF, 0xE) => {
+                let byte = self.fetch(io);
+                debug_println!("CPI #${:02X}", byte);
+
+                let (res, cy) = self.register_read(Register::A).overflowing_sub(byte);
+                self.update_flag_z(res);
+                self.update_flag_s(res);
+                self.update_flag_p(res);
+                self.update_flag(Flag::C, cy);
             }
 
             // LXI
@@ -147,7 +192,7 @@ impl Cpu8080 {
                 } else {
                     MemReg::Register(Register::from(dst))
                 };
-                self.move_to(io, dst, byte);
+                self.location_write(io, dst, byte);
 
                 debug_println!("MVI {:?}, #${:02X}", dst, byte);
             }
@@ -169,8 +214,8 @@ impl Cpu8080 {
                     MemReg::Register(Register::from(src))
                 };
 
-                let byte = self.move_from(io, src);
-                self.move_to(io, dst, byte);
+                let byte = self.location_read(io, src);
+                self.location_write(io, dst, byte);
 
                 debug_println!("MOV {:?}, {:?}", dst, src);
             }
@@ -190,6 +235,50 @@ impl Cpu8080 {
                 let dst = [B, D, H, SP][op_hi as usize];
                 let word = self.register_read_word(dst);
                 self.register_write_word(dst, word.wrapping_add(1));
+
+                debug_println!("INX {:?}", dst);
+            }
+
+            // DCR
+            (0x0..=0x3, 0x5 | 0xD) => {
+                let dst = (op >> 3) & 0x7;
+                let dst = if dst == 6 {
+                    MemReg::Memory
+                } else {
+                    MemReg::Register(Register::from(dst))
+                };
+
+                let byte = self.location_read(io, dst);
+                let new_value = byte.wrapping_sub(1);
+                self.location_write(io, dst, new_value);
+
+                self.update_flag_z(new_value);
+                self.update_flag_s(new_value);
+                self.update_flag_p(new_value);
+
+                debug_println!("DCR {:?}", dst);
+            }
+
+            // PUSH
+            (0xC..=0xF, 0x5) => {
+                use Register::*;
+                let src = [B, D, H, A][(op_hi - 0xC) as usize];
+
+                debug_println!("PUSH {:?}", src);
+
+                let word = self.register_read_word(src);
+                self.push(io, word);
+            }
+
+            // POP
+            (0xC..=0xF, 0x1) => {
+                use Register::*;
+                let dst = [B, D, H, A][(op_hi - 0xC) as usize];
+
+                debug_println!("POP {:?}", dst);
+
+                let word = self.pop(io);
+                self.register_write_word(dst, word);
             }
 
             _ => {
@@ -199,8 +288,34 @@ impl Cpu8080 {
         }
     }
 
+    fn update_flag_z(&mut self, value: Byte) {
+        self.update_flag(Flag::Z, value == 0);
+    }
+
+    fn update_flag_s(&mut self, value: Byte) {
+        self.update_flag(Flag::S, value & (1 << 7) > 0);
+    }
+
+    fn update_flag_p(&mut self, value: Byte) {
+        self.update_flag(Flag::P, value.count_ones() % 2 == 0);
+    }
+
+    /// Set a CPU flag to the given value
+    fn update_flag(&mut self, flag: Flag, value: bool) {
+        if value {
+            self.flags |= flag as Byte;
+        } else {
+            self.flags &= !(flag as Byte);
+        }
+    }
+
+    /// Get the value of a CPU flag
+    fn get_flag(&mut self, flag: Flag) -> bool {
+        (self.flags & (flag as Byte)) != 0
+    }
+
     /// Move data into a register or memory location (for MOV, MVI, etc)
-    fn move_to<IO: IOManager>(&mut self, io: &mut IO, dst: MemReg, byte: Byte) {
+    fn location_write<IO: IOManager>(&mut self, io: &mut IO, dst: MemReg, byte: Byte) {
         match dst {
             MemReg::Memory => {
                 let addr = self.register_read_word(Register::H);
@@ -213,7 +328,7 @@ impl Cpu8080 {
     }
 
     /// Move data from a register or memory location (for MOV, MVI, etc)
-    fn move_from<IO: IOManager>(&mut self, io: &mut IO, src: MemReg) -> Byte {
+    fn location_read<IO: IOManager>(&mut self, io: &mut IO, src: MemReg) -> Byte {
         match src {
             MemReg::Memory => {
                 let addr = self.register_read_word(Register::H);
@@ -229,11 +344,19 @@ impl Cpu8080 {
     }
 
     /// Push a word onto the stack
-    fn push_word<IO: IOManager>(&mut self, io: &mut IO, word: Word) {
+    fn push<IO: IOManager>(&mut self, io: &mut IO, word: Word) {
         for b in word.to_be_bytes() {
             io.write(self.sp, b);
             self.sp -= 1;
         }
+    }
+
+    /// Pop a word from the stack
+    fn pop<IO: IOManager>(&mut self, io: &IO) -> Word {
+        u16::from_le_bytes([(); 2].map(|_| {
+            self.sp += 1;
+            io.read(self.sp)
+        }))
     }
 
     /// Fetch a single byte from memory indexed by the program counter and then
@@ -271,6 +394,10 @@ impl Cpu8080 {
                 self.register_write(H, hi);
                 self.register_write(L, lo);
             }
+            A => {
+                self.register_write(A, hi);
+                self.flags = lo;
+            }
             SP => self.sp = word,
             _ => panic!("Invalid register pair `{:?}'", dst),
         }
@@ -299,6 +426,11 @@ impl Cpu8080 {
             H => {
                 let hi = self.register_read(H);
                 let lo = self.register_read(L);
+                u16::from_be_bytes([hi, lo])
+            }
+            A => {
+                let hi = self.register_read(A);
+                let lo = self.flags;
                 u16::from_be_bytes([hi, lo])
             }
             SP => self.sp,
@@ -354,6 +486,19 @@ mod tests {
     }
 
     #[test]
+    fn inst_jnz() {
+        for addr in [0xABCDu16, 0x1000] {
+            for (z, exp_pc) in [(true, 0x0003), (false, addr)] {
+                let [hi, lo] = addr.to_be_bytes();
+                let (mut cpu, mut io) = make_test_cpu(vec![0xC2, lo, hi], None);
+                cpu.update_flag(Flag::Z, z);
+                cpu.step(&mut io);
+                assert_eq!(cpu.pc, exp_pc);
+            }
+        }
+    }
+
+    #[test]
     fn inst_call() {
         {
             let (mut cpu, mut io) = make_test_cpu(vec![0xCD, 0x10, 0x00], Some(0x100));
@@ -372,6 +517,34 @@ mod tests {
             assert_eq!(cpu.pc, 0xABCD, "new pc");
             assert_eq!(io.read(cpu.sp + 2), 0x00, "return address hi");
             assert_eq!(io.read(cpu.sp + 1), 0x03, "return address lo");
+        }
+    }
+
+    #[test]
+    fn inst_ret() {
+        for addr in [0xABCD, 0x1000, 0x0000] {
+            let (mut cpu, mut io) = make_test_cpu(vec![0xC9], Some(0x100));
+            cpu.sp = 0xFF;
+            cpu.push(&mut io, addr);
+            cpu.step(&mut io);
+            assert_eq!(cpu.pc, addr);
+        }
+    }
+
+    #[test]
+    fn inst_cpi() {
+        for (val, cmp_val, z, s, c, p) in [
+            (10, 10, true, false, false, true),
+            (10, 5, false, false, false, true),
+            (10, 15, false, true, true, false),
+        ] {
+            let (mut cpu, mut io) = make_test_cpu(vec![0xFE, cmp_val], None);
+            cpu.register_write(Register::A, val);
+            cpu.step(&mut io);
+            assert_eq!(cpu.get_flag(Flag::Z), z, "{} = {}, Z", val, cmp_val);
+            assert_eq!(cpu.get_flag(Flag::S), s, "{} = {}, S", val, cmp_val);
+            assert_eq!(cpu.get_flag(Flag::C), c, "{} = {}, C", val, cmp_val);
+            assert_eq!(cpu.get_flag(Flag::P), p, "{} = {}, P", val, cmp_val);
         }
     }
 
@@ -453,10 +626,10 @@ mod tests {
                 let (mut cpu, mut io) = make_test_cpu(vec![op], Some(65536));
                 io.write(0x0010, val);
                 cpu.register_write_word(Register::H, 0x0010);
-                cpu.move_to(&mut io, src, val);
+                cpu.location_write(&mut io, src, val);
                 cpu.step(&mut io);
                 assert_eq!(
-                    cpu.move_from(&mut io, dst),
+                    cpu.location_read(&mut io, dst),
                     val,
                     "({:02X}) MOV {:?}, {:?}",
                     op,
@@ -496,6 +669,75 @@ mod tests {
                     val,
                     exp
                 );
+            }
+        }
+    }
+
+    #[test]
+    fn inst_dcr() {
+        use super::Register::*;
+        use MemReg::*;
+
+        for (loc, dcr) in [
+            (Register(B), 0x05),
+            (Register(C), 0x0D),
+            (Register(D), 0x15),
+            (Register(E), 0x1D),
+            (Register(H), 0x25),
+            (Register(L), 0x2D),
+            (Memory, 0x35),
+            (Register(A), 0x3D),
+        ] {
+            for (val, exp, z, s, p) in [
+                (0, 255, false, true, true),
+                (255, 254, false, true, false),
+                (1, 0, true, false, true),
+            ] {
+                let (mut cpu, mut io) = make_test_cpu(vec![dcr], Some(0x20));
+                cpu.register_write_word(H, 0x0010);
+                cpu.location_write(&mut io, loc, val);
+                cpu.step(&mut io);
+                assert_eq!(
+                    cpu.location_read(&mut io, loc),
+                    exp,
+                    "DCR {:?} ({} -> {})",
+                    loc,
+                    val,
+                    exp
+                );
+                assert_eq!(cpu.get_flag(Flag::Z), z, "Z");
+                assert_eq!(cpu.get_flag(Flag::S), s, "S");
+                assert_eq!(cpu.get_flag(Flag::P), p, "P");
+            }
+        }
+    }
+
+    #[test]
+    fn inst_push_pop() {
+        use Register::*;
+        for (reg, push, pop) in [
+            (B, 0xC5, 0xC1),
+            (D, 0xD5, 0xD1),
+            (H, 0xE5, 0xE1),
+            (A, 0xF5, 0xF1),
+        ] {
+            for val in [0xABCDu16, 0x0000, 0x1000] {
+                let (mut cpu, mut io) = make_test_cpu(vec![push, pop], Some(0x100));
+                let [hi, lo] = val.to_be_bytes();
+
+                cpu.sp = 0xFF;
+                cpu.register_write_word(reg, val);
+
+                cpu.step(&mut io);
+                assert_eq!(io.read(cpu.sp + 1), lo, "PUSH {:?} (val={:04X})", reg, val);
+                assert_eq!(io.read(cpu.sp + 2), hi, "PUSH {:?} (val={:04X})", reg, val);
+
+                // overwrite with dummy value
+                cpu.register_write_word(reg, 0xD8D8);
+
+                cpu.step(&mut io);
+
+                assert_eq!(cpu.register_read_word(reg), val, "POP {:?}", reg,);
             }
         }
     }
